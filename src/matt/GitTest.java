56d7c0796a3bd5c237b85c3e6cfd1255cdce8931
Matt Bao <bobconan@gmail.com>
2015-04-21 00:30:40 -0700

    Commite changes

--- src/matt/GitTest.java
+++ src/matt/GitTest.java
@@ -2,8 +2,12 @@ package matt;
 
 public class GitTest {
 	
+	// Test for GitHub
+	
 	public static void main(String[] args){
-		System.out.println("GitHub HelloWorld!");
+		
+		System.out.println("GitHub HelloWorld2!");
+		System.out.println("GitHub HelloWorld3!");
 	}
 	
 }
